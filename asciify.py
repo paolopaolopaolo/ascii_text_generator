9a492fcd15dd8a813fcd3f6190c6819b24ea7f20
Dean Mercado <dean@yeti.co>
2016-08-10 22:56:22 -0700

    Release-0.0.2
    - Create modules that do the asciification
    - Add boilerplate to make "asciify" a PyPI package
    - Add `click` to make `asciify` a CLI tool

--- asciify.py
+++
@@ -1,9 +0,0 @@
-from word_renderer import WordRenderer
-
-def main(word, positive='O', negative=' '):
-	renderer = WordRenderer()
-	print renderer.create_rendered_word(word, positive, negative)
-
-if __name__ == '__main__':
-	import sys
-	main(*sys.argv[1:])
