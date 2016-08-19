79acd34b39884462142b51c0b31bc30495b7fc23
Dean Mercado <dean@yeti.co>
2016-08-18 17:54:12 -0700

    Release-0.0.3
    - Update letters `Mm`, `Ww`, `l`, `i`, `n`, `o`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="asciify",
     packages=find_packages(),
-    version="0.0.2",
+    version="0.0.3",
     description="Fun little project",
     long_description="",
     url="https://github.com/paolopaolopaolo/ascii_text_generator/",
